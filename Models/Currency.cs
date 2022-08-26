fcb747fff881ca48a1fc8ba62db71dbf947ce7f3
Pa33oP <worldguardmc@gmail.com>
2022-08-26 13:03:56 +0300

    ValueRefTypes: Создал сущности и методы

---
+++ Models/Currency.cs
@@ -0,0 +1,8 @@
+﻿namespace Models
+{
+    public struct Currency 
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ PracticeWithTypes/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Models;
+
+namespace PractiseWithTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //контракт
+            Employee employee = new Employee();
+            employee.Contract = "Контракт от 25.05.2021";
+            Console.WriteLine(employee.Contract);
+
+            WrongUpdateEmployeeContract(employee);
+            Console.WriteLine(employee.Contract);
+
+            employee.Contract = UpdateEmployeeContract(employee.Contract);
+            Console.WriteLine(employee.Contract);
+
+            //валюта
+            Currency currency = new Currency();
+            currency.Name = "Рубль ПМР";
+            Console.WriteLine(currency.Name);
+
+            WrongUpdateCurrency(currency);
+            Console.WriteLine(currency.Name);
+
+            currency.Name = RightUpdateCurrency(currency.Name);
+            Console.WriteLine(currency.Name);
+
+            Console.ReadKey();
+        }
+
+        static void WrongUpdateEmployeeContract(Employee employee)
+        {
+            employee.Contract = "Контракт от 25.05.2022";
+        }
+
+        static string UpdateEmployeeContract(string contract)
+        {
+            return "Контракт от 25.05.2022";
+        }
+
+        static void WrongUpdateCurrency(Currency currency)
+        {
+            currency.Name = "Рубль РФ";
+        }
+
+        static string RightUpdateCurrency(string currencyName)
+        {
+            return currencyName = "Рубль РФ";
+        }
+    }
+}
